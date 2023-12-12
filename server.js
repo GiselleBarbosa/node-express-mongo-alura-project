6c8c7b3c7529f4a8e41555b21d9d38a59eba1fd5
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-12 17:27:10 -0300

    criando rotas

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+	"/": "Curso de Node.JS",
+};
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { "content-type": "text/plain" });
-	res.end("curso de Node.js");
+	res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
