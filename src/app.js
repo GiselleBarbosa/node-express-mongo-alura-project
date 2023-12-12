074f8a02bfc3b8c81d6e2164b23c169567ec10b9
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-12 20:46:45 -0300

    adicionado metodo get via express

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ import express from "express";
 
 const app = express();
 
+const clientes = [
+	{ id: 1, nome: "maria", idade: 56 },
+	{ id: 2, nome: "jose", idade: 35 },
+	{ id: 3, nome: "joaozinho", idade: 8 },
+];
+
 app.get("/", (req, res) => {
-	res.status(200).send("Cadastro de Clientes");
+	res.status(200).send("Cadastro de Clientes - Pagina inicial");
+});
+
+app.get("/clientes", (req, res) => {
+	res.status(200).json(clientes);
 });
 
 export default app;
