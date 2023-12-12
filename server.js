9472d1958deb3f20a6026e96dc19cc855918a151
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-12 20:17:00 -0300

    instalado e configurado express

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const routes = {
-	"/home": "Pagina inicial",
-	"/clientes": "Esta e a lista de clientes",
-	"/clientes/id": "Rota de clientes por ID"
-};
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { "content-type": "text/plain" });
-	res.end(routes[req.url]);
-});
-
-server.listen(PORT, () => {
-	console.log("servidor escutando na porta 3000");
+app.listen(PORT, () => {
+	console.log("servidor rodando na porta 3000");
 });
