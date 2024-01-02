40f3a09c7556d9ffacfa440c3e8e50e94c195714
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-02 19:00:19 -0300

    adicionadas novas rotas

--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -3,6 +3,10 @@ import express from "express";
 
 const routes = express.Router();
 
-routes.get("/clientes", ClientController.getClients);
+routes.get("/clientes", ClientController.readClients);
+routes.get("/clientes/:id", ClientController.readClientByID);
+routes.post("/clientes", ClientController.createClient);
+routes.put("/clientes", ClientController.updateClient);
+routes.delete("/clientes/:id", ClientController.removeClient);
 
 export default routes;
