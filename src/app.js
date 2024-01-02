4fec5e12dc769b1726dcf8b8c6ca57a6c1ccf822
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-02 19:00:46 -0300

    refac metodos de criar, atualizar, pegar, pegar por ID e remover

--- src/app.js
+++ src/app.js
@@ -15,21 +15,4 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/cliente/:id", (req, res) => {
-	const index = buscaClientePorID(req.params.id);
-	res.status(200).json(clientes[index]);
-});
-
-app.put("/cliente/:id", (req, res) => {
-	const index = buscaClientePorID(req.params.id);
-	clientes[index].nome = req.body.nome;
-	res.status(200).json(clientes);
-});
-
-app.delete("/cliente/:id", (req, res) => {
-	const index = buscaClientePorID(req.params.id);
-	clientes.splice(index, 1);
-	res.status(200).send(clientes);
-});
-
 export default app;
--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -1,12 +1,30 @@
 import client from "../models/client.model.js";
 
 class ClientController {
-	static async getClients(req, res) {
-		const list = await client.find({});
-		res.status(200).json(list);
+	static async readClients(req, res) {
+		try {
+			const list = await client.find({});
+			res.status(200).json(list);
+		} catch (error) {
+			res.status(500).json({
+				message: `${error.message} - falha na requisição`,
+			});
+		}
 	}
 
-	static async registerClient(req, res) {
+	static async readClientByID(req, res) {
+		try {
+			const id = req.params.id;
+			const clientFound = await client.findById(id);
+			res.status(200).json(clientFound);
+		} catch (error) {
+			res.status(500).json({
+				message: `${error.message} - falha na requisição do cliente`,
+			});
+		}
+	}
+
+	static async createClient(req, res) {
 		try {
 			const newClient = await client.create(req.body);
 			res.status(201).json({
@@ -16,6 +34,34 @@ class ClientController {
 		} catch (error) {
 			res.status(500).json({
 				message: `${error.message} - falha ao cadastrar cliente.`,
+			});
+		}
+	}
+
+	static async updateClient(req, res) {
+		try {
+			const id = req.params.id;
+			await client.findByIdAndUpdate(id, req.body);
+			res.status(200).json({
+				message: "Cliente atualizado com sucesso.",
+			});
+		} catch (error) {
+			res.status(500).json({
+				message: `${error.message} - falha na atualização do cliente`,
+			});
+		}
+	}
+
+	static async removeClient(req, res) {
+		try {
+			const id = req.params.id;
+			await client.findByIdAndDelete(id);
+			res.status(200).json({
+				message: "Cliente excluído com sucesso.",
+			});
+		} catch (error) {
+			res.status(500).json({
+				message: `${error.message} - falha na remoção do cliente`,
 			});
 		}
 	}
